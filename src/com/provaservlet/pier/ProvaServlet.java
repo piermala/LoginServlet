475b8837fe0ba9b5b4415aa37962977bb6958e25
Pierangelo <pierangelo92@live.it>
2016-10-18 09:45:48 +0200

    carico LoginServlet

---
+++ src/com/provaservlet/pier/ProvaServlet.java
@@ -0,0 +1,48 @@
+package com.provaservlet.pier;
+
+import java.io.IOException;
+import java.io.PrintWriter;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
+
+/**
+ * Servlet implementation class ProvaServlet
+ */
+@WebServlet("/ProvaServlet")
+public class ProvaServlet extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+       
+   
+	/**
+	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		response.setContentType("text/html");
+		
+		String nome = request.getParameter("nome");
+		
+		HttpSession session = request.getSession();
+		
+		PrintWriter writer = response.getWriter();
+		
+		if(nome != null && !nome.isEmpty()){
+			writer.println("Benvenuto " + nome +"!");
+		} else {
+			response.sendRedirect("login.html");
+		}
+		
+	}
+
+	/**
+	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		
+	}
+
+}
